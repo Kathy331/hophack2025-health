87839f3453ebe0329150c1e6293ca0c1e424a6b6
Kathy Chen <Kathychen331@outlook.com>
2025-09-13 05:47:19 -0400

    Revert "Merge branch 'trina' of https://github.com/Kathy331/hophack2025-health into Kathy-recipes4"
    This reverts commit 6b478789bd0f19b0d0fa38ba0b0dbacc327e637a, reversing
    changes made to 381653195fb237a36530c9af1563c46eca5573f5.

--- frontend/app/(tabs)/(recipes)/_layout.tsx
+++ frontend/app/(tabs)/(recipes)/_layout.tsx
@@ -1,40 +1,9 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View, Text } from 'react-native';
 
 export default function CommunityLayout() {
   return (
-    
-    <View style={styles.container}>
-       <View style={styles.headerBg}>
-          <Text style={styles.headerTxt}>Recipes</Text>
-        </View>
-      <View style={{
-        width: 350,
-        height: 700,
-        backgroundColor: '#fffcfcff',
-      }}>
-      </View>
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'recipes' }} />
+    </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f7f1f1ff',
-    justifyContent: 'center',
-        alignItems: 'center'
-  },
-  headerTxt:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  headerBg:{
-    backgroundColor: '#3c7746ff',
-    padding: 10,
-
-  },
-
- 
-});
--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -4,12 +4,8 @@ import { Platform } from 'react-native';
 
 import { HapticTab } from '../../components/haptic-tab';
 import { IconSymbol } from '../../components/ui/icon-symbol';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from '../../constants/theme';
 import { useColorScheme } from '../../hooks/use-color-scheme';
-import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -28,6 +24,13 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
+        name="(add)"
+        options={{
+          title: 'Add',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="(home)"
         options={{
           title: 'Home',
@@ -38,28 +41,22 @@ export default function TabLayout() {
         name="(inventory)"
         options={{
           title: 'Inventory',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="food-apple-outline" size={24} color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+
       <Tabs.Screen
-        name="(add)"
+        name="(profile)"
         options={{
-          title: 'Add',
-          tabBarIcon: ({ color }) => <AntDesign size={24} name="plus-circle" color={color} />,
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="(recipes)"
         options={{
           title: 'Recipes',
-          tabBarIcon: ({ color }) => <MaterialIcons name="list-alt" size={24} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="(profile)"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <Ionicons name="person-sharp" size={24} color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       
