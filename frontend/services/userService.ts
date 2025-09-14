a7a4d1a40003e89bea4ab900ca13c3535b0c80b2
Kathy Chen <Kathychen331@outlook.com>
2025-09-13 21:41:34 -0400

    kathy's set up

--- frontend/services/userService.ts
+++ frontend/services/userService.ts
@@ -1,4 +1,4 @@
-const backendUrl = "https://c334963bcfc2.ngrok-free.app"; // replace with your backend
+const backendUrl = "https://2e31f671637a.ngrok-free.app"; // replace with your backend
 
 export interface Recipe {
   id?: number;  // recipe_id from database
