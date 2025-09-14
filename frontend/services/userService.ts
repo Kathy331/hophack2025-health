1367952af7a1a8baf9a82aed729861e80a7cc862
toast-07 <92638153+toast-07@users.noreply.github.com>
2025-09-13 21:43:14 -0400

    endpoint fix

--- frontend/services/userService.ts
+++ frontend/services/userService.ts
@@ -1,4 +1,4 @@
-const backendUrl = "https://1f517d5f5d00.ngrok-free.app"; // replace with your backend
+const backendUrl = "https://d647843159d4.ngrok-free.app"; // replace with your backend
 
 export interface Recipe {
   id?: number;  // recipe_id from database
